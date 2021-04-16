4a406397ce07810d6715631e4b90a9755ebcaa67
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-16 18:40:52 +0100

    fixed router with authentication ready

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,81 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "@/views/Login.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import( '../views/About.vue')
-  }
-]
+    path: "/register",
+    name: "About",
+    component: () => import("../views/Register.vue"),
+  },
+];
+
+// let router = new VueRouter({
+//   routes: [
+//     {
+//       path: "/dash",
+//       name: "dashboard",
+//       component: () => import("../views/Dashboard.vue"),
+//       meta: {
+//         requiresAuth: true,
+//       },
+//     },
+//     {
+//       path: "/",
+//       name: "login",
+//       component: Login,
+//       meta: {
+//         requiresGuest: true,
+//       },
+//     },
+//     {
+//       path: "/register",
+//       name: "register",
+//       component: () => import("../views/Register.vue"),
+//       meta: {
+//         requiresGuest: true,
+//       },
+//     },
+//   ],
+// });
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some((record) => record.meta.requiresAuth)) {
+//     if (!firebase.auth().currentUser) {
+//       next({
+//         path: "/",
+//         query: {
+//           redirect: to.fullPath,
+//         },
+//       });
+//     } else {
+//       next();
+//     }
+//   } else if (to.matched.some((record) => record.meta.requiresGuest)) {
+//     if (firebase.auth().currentUser) {
+//       next({
+//         path: "/dash",
+//         query: {
+//           redirect: to.fullPath,
+//         },
+//       });
+//     } else {
+//       next();
+//     }
+//   } else {
+//     next();
+//   }
+// });
+
+export default router;
