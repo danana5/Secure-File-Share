1998dffc520dda73273b26d3943a3f72f037809b
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-16 18:38:44 +0100

    created template view pages with login page fron end sorted

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Dashboard</h1>
+</template>
+<script>
+import db from "../firebaseInit"
+export default {
+    created(){
+        console.log(db)
+    }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+<v-container>
+    <v-card class="mx-auto mt-10" max-width="600">
+        <v-container>
+            <form>
+                <h2 style="text-align: center;">Log In</h2>
+                <v-divider></v-divider>
+                <v-text-field color="#0D47A1" class="mt-5" v-model="email" label="Email" outlined>
+                </v-text-field>
+                <v-text-field color="#0D47A1" v-model="password" outlined :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" label="Password" @click:append="show = !show"></v-text-field>
+                <v-checkbox v-checkbox v-model="rememberME" label="Stay Logged in?"></v-checkbox>
+                <v-btn class="white--text" color="blue darken-3" outlined rounded @click="login">
+                    Login
+                </v-btn>
+            </form>
+        </v-container>
+    </v-card>
+</v-container>
+</template>
+
+<script>
+import firebase from "firebase"
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            show: false,
+            rememberME: false,
+            failed: false,
+            loading: false,
+            logged: true
+        }
+    },
+    methods: {
+        login() {
+            this.loading = true
+            if (this.rememberME) {
+                firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                    .then(user => {
+                            console.log("Logged in as " + user.email)
+                            this.$router.push("/dash")
+                            this.loading = false
+                            this.logged = false
+                        },
+                        err => {
+                            alert(err.message)
+                            this.loading = false
+                        })
+            } else {
+                firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                    .then(user => {
+                            console.log("Logged in as " + user.email)
+                            this.$router.push("/dash")
+                            this.loading = false
+                            this.logged = false
+                        },
+                        err => {
+                            alert(err.message)
+                            this.loading = false
+                        })
+            }
+        }
+    }
+}
+</script>
