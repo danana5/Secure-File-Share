0f0d2769817d6627d6133399caf6b5744093ca2a
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-16 21:02:04 +0100

    added components folder with navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+<v-app-bar app color="#187789" extension-height="7" extended>
+    <v-app-bar-nav-icon color="#76FF03" class="mt-2"></v-app-bar-nav-icon>
+    <v-app-bar-title>
+        <v-img src="@/assets/logo.png" class="mt-4">
+        </v-img>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <h1 v-if="online">online</h1>
+</v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    props: [online]
+}
+</script>
+
+<style>
+
+</style>
