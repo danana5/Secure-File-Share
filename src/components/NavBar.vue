879aa8cc7806377f6d86d8b31705316117cbf545
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-23 00:56:39 +0100

    updated navbar to display current user

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,22 +2,26 @@
 <v-app-bar app color="#187789" extension-height="7" extended>
     <v-app-bar-nav-icon color="#76FF03" class="mt-2"></v-app-bar-nav-icon>
     <v-app-bar-title>
-        <v-img src="@/assets/logo.png" class="mt-4">
-        </v-img>
+        <router-link to="/dash"><v-img src="@/assets/logo2.png" >
+        </v-img></router-link>
     </v-app-bar-title>
     <v-spacer></v-spacer>
-    <div v-if="online">
-        <h3 style="color:#76FF03">online <v-icon>mdi-wifi</v-icon>
+
+    <div v-if="online" class="mt-2">
+        <h3 style="color:#76FF03">online <v-icon color="white">mdi-wifi</v-icon>
         </h3>
     </div>
-    <div v-else>
-        <h3 style="color:#76FF03">offline <v-icon>mdi-wifi-off</v-icon>
+    <div v-else class="mt-2">
+        <h3 style="color:#76FF03">offline <v-icon color="white">mdi-wifi-off</v-icon>
         </h3>
+    </div>
+    <div v-if="loggedIn" class="ml-2 mt-2">
+        <h4 style="color:#76FF03">{{user}}</h4>
     </div>
 
     <div v-if="loggedIn">
-        <v-btn class="ml-2" rounded outlined @click="logout()" color="#76FF03">
-            Log Out<v-icon>mdi-logout</v-icon>
+        <v-btn class="ml-2 mt-2" rounded dark @click="logout()" color="#76FF03">
+            Log Out <v-icon>mdi-logout</v-icon>
         </v-btn>
     </div>
 </v-app-bar>
@@ -34,18 +38,23 @@ export default {
                 return {}
             }
         },
-        loggedIn:{
+        loggedIn: {
             type: Boolean,
-            default: function(){
-                return{}
+            default: function () {
+                return {}
+            }
+        },
+        user: {
+            type: String,
+            default: function () {
+                return {}
             }
         }
     },
     methods: {
         logout() {
             firebase.auth().signOut().then(() => {
-                this.loggedIn = false
-                this.$router.push(`/login`)
+                this.$router.push(`/log-in`)
             })
         }
     }
