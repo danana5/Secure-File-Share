c4842b624dbb2b36ef7fbf29527dc7bf5334f9ee
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-23 00:59:13 +0100

    adding new users to the database

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,7 +8,7 @@
             </v-text-field>
             <v-text-field color="#76FF03" v-model="password" outlined :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" label="Password" @click:append="show = !show"></v-text-field>
             <v-text-field hide-details color="#76FF03" v-model="password2" outlined :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :type="show1 ? 'text' : 'password'" label="Retype Password" @click:append="show1 = !show1"></v-text-field>
-            <p class="mt-1">Already own an account? <router-link to="/login" style="text-decoration-color: #76FF03; color: #76FF03">Log In Here</router-link>
+            <p class="mt-1">Already own an account? <router-link to="/log-in" style="text-decoration-color: #76FF03; color: #76FF03">Log In Here</router-link>
             </p>
             <v-row justify="center" class="mb-1 mt-2">
                 <v-btn color="#76FF03" outlined rounded @click="register">
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import db from "../firebaseInit"
 import firebase from "firebase"
 export default {
     data() {
@@ -38,26 +39,28 @@ export default {
     },
     methods: {
         register: function (e) {
-            if(this.password == this.password2){
+            if (this.password == this.password2) {
                 firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(user => {
-                    console.log("account created for " + user.email)
-                    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-                    firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-                        .then(u => {
-                                console.log("Logged in as " + u.email)
-                                this.$router.push("/dash")
-                            },
-                            err => {
-                                alert(err.message)
-                                this.loading = false
-                            })
-                },
-                err => {
-                    alert(err.message)
-                })
-            e.preventDefault();
-            }
-            else{
+                        db.collection("users").add({
+                            UserID: user.user.uid,
+                            email: user.user.email
+                        })
+                        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+                        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                            .then(u => {
+                                    console.log("Logged in as " + u.user.email)
+                                    this.$router.push("/dash")
+                                },
+                                err => {
+                                    alert(err.message)
+                                    this.loading = false
+                                })
+                    },
+                    err => {
+                        alert(err.message)
+                    })
+                e.preventDefault();
+            } else {
                 this.failed = true
             }
         }
