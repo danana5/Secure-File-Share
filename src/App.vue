05637f41f8b532a902f67447b1c10321d888aa57
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-22 21:52:24 +0100

    added user logged in check for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <v-app>
-    <NavBar :online="online"/>
+<v-app>
+    <NavBar :online="online" :loggedIn="loggedIn" />
     <v-main style="background:#BDBDBD">
-      <router-view></router-view>
+        <router-view></router-view>
     </v-main>
-  </v-app> 
+</v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar"
+import firebase from "firebase"
 import db from "./firebaseInit"
 export default {
-  name: 'App',
-  components:{
-    NavBar
-  },
-
-  data: () => ({
-    online: false,
-  }),
-  computed:{
-    online(){
-      this.online = true
-      return this.online;
+    name: 'App',
+    components: {
+        NavBar
+    },
+    data: () => ({
+        online: false,
+        loggedIn: false
+    }),
+    watch: {
+        $route() {
+            if(firebase.auth().currentUser){
+                this.loggedIn = true
+            }
+            else{
+                this.loggedIn = false
+            }
+        }
+    },
+    mounted() {
+      console.log(db)
+        if (db != null) {
+            this.online = true
+        } else {
+            this.online = false
+        }
+        return this.online;
     }
-  }
 };
 </script>
