54de3cf20c7c91e90c8ac0d1e49590aa265da812
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-22 21:52:49 +0100

    added user checking for log out button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,14 +6,49 @@
         </v-img>
     </v-app-bar-title>
     <v-spacer></v-spacer>
-    <h1 v-if="online">online</h1>
+    <div v-if="online">
+        <h3 style="color:#76FF03">online <v-icon>mdi-wifi</v-icon>
+        </h3>
+    </div>
+    <div v-else>
+        <h3 style="color:#76FF03">offline <v-icon>mdi-wifi-off</v-icon>
+        </h3>
+    </div>
+
+    <div v-if="loggedIn">
+        <v-btn class="ml-2" rounded outlined @click="logout()" color="#76FF03">
+            Log Out<v-icon>mdi-logout</v-icon>
+        </v-btn>
+    </div>
 </v-app-bar>
 </template>
 
 <script>
+import firebase from "firebase"
 export default {
     name: "NavBar",
-    props: [online]
+    props: {
+        online: {
+            type: Boolean,
+            default: function () {
+                return {}
+            }
+        },
+        loggedIn:{
+            type: Boolean,
+            default: function(){
+                return{}
+            }
+        }
+    },
+    methods: {
+        logout() {
+            firebase.auth().signOut().then(() => {
+                this.loggedIn = false
+                this.$router.push(`/login`)
+            })
+        }
+    }
 }
 </script>
 
