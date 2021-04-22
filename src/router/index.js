1200ab004a905c3e2741c0012c7a1e1f99649fc0
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-22 21:53:08 +0100

    added nav guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,85 +1,86 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import firebase from "firebase";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Login",
-    component: () => import("@/views/Login.vue"),
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: () => import("@/views/Register.vue"),
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: () => import("@/views/Dashboard.vue"),
-  },
-];
+// const routes = [
+//   {
+//     path: "/login",
+//     name: "Login",
+//     component: () => import("@/views/Login.vue"),
+//   },
+//   {
+//     path: "/",
+//     name: "Register",
+//     component: () => import("@/views/Register.vue"),
+//   },
+//   {
+//     path: "/dashboard",
+//     name: "Dashboard",
+//     component: () => import("@/views/Dashboard.vue"),
+//   },
+// ];
 
-// let router = new VueRouter({
-//   routes: [
-//     {
-//       path: "/dash",
-//       name: "dashboard",
-//       component: () => import("../views/Dashboard.vue"),
-//       meta: {
-//         requiresAuth: true,
-//       },
-//     },
-//     {
-//       path: "/",
-//       name: "login",
-//       component: Login,
-//       meta: {
-//         requiresGuest: true,
-//       },
-//     },
-//     {
-//       path: "/register",
-//       name: "register",
-//       component: () => import("../views/Register.vue"),
-//       meta: {
-//         requiresGuest: true,
-//       },
-//     },
-//   ],
-// });
-
-const router = new VueRouter({
-  routes,
+let router = new VueRouter({
+  routes: [
+    {
+      path: "/dash",
+      name: "dashboard",
+      component: () => import("../views/Dashboard.vue"),
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/Login.vue"),
+      meta: {
+        requiresGuest: true,
+      },
+    },
+    {
+      path: "/",
+      name: "register",
+      component: () => import("../views/Register.vue"),
+      meta: {
+        requiresGuest: true,
+      },
+    },
+  ],
 });
 
-// router.beforeEach((to, from, next) => {
-//   if (to.matched.some((record) => record.meta.requiresAuth)) {
-//     if (!firebase.auth().currentUser) {
-//       next({
-//         path: "/",
-//         query: {
-//           redirect: to.fullPath,
-//         },
-//       });
-//     } else {
-//       next();
-//     }
-//   } else if (to.matched.some((record) => record.meta.requiresGuest)) {
-//     if (firebase.auth().currentUser) {
-//       next({
-//         path: "/dash",
-//         query: {
-//           redirect: to.fullPath,
-//         },
-//       });
-//     } else {
-//       next();
-//     }
-//   } else {
-//     next();
-//   }
+// const router = new VueRouter({
+//   routes,
 // });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!firebase.auth().currentUser) {
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some((record) => record.meta.requiresGuest)) {
+    if (firebase.auth().currentUser) {
+      next({
+        path: "/dash",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router;
