ac0836cad80679a7bdf503d7d4a0775e54fa77ed
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-23 17:47:35 +0100

    started implementation of group view

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -1,10 +1,31 @@
 <template>
-  <h1>groups</h1>
+  <v-container>
+    <v-card>
+      <v-container>
+        <h1>{{group.name}}</h1>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import db from "../firebaseInit"
 export default {
-
+  data(){
+    return{
+      group: {
+        "name": "",
+        "users" : []
+      }
+    }
+  },
+  created(){
+    const groupID = this.$route.params.group_id
+    console.log(groupID)
+    db.collection("groups").doc(groupID).get().then(doc =>{
+      this.group.name = doc.data().name
+    })
+  }
 }
 </script>
 
