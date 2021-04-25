cc66ef799152ccf1ac0ceeb3f04a1d909a1b7854
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-25 19:38:33 +0100

    encryption and added downloading of files

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -15,19 +15,16 @@
             <v-container>
                 <v-list>
                     <h3>Files</h3>
-                    <v-list-item v-for="(file, i) in group.files" :key="i">
+                    <v-list-item v-for="(file, i) in group.files" :key="i" @click="download(file)">
                         <v-list-item-avatar>
                             <v-icon>
-                                mdi-file
+                                mdi-file-cloud
                             </v-icon>
                         </v-list-item-avatar>
                         <v-list-item-content>
                             <v-list-item-title>
-                                {{file.name}}
+                                {{file}}
                             </v-list-item-title>
-                            <v-list-item-subtitle>
-                                {{file.size}}
-                            </v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </v-list>
@@ -38,8 +35,11 @@
 </template>
 
 <script>
-import db from "../firebaseInit";
+import db from "../firebaseInit"
 import firebase from "firebase"
+import {Crypt} from "hybrid-crypto-js";
+import fileDownload from "js-file-download";
+import CryptoJS from "crypto-js";
 export default {
     data() {
         return {
@@ -68,20 +68,87 @@ export default {
         upload() {
             if (this.chosenFile != null) {
                 this.path = `${this.$route.params.group_id}/${this.chosenFile.name}`
-                firebase.storage().ref().child(this.path).put(this.chosenFile).then(() => {
-                    this.group.files = []
-                    this.getFiles()
+                db.collection("groups").doc(this.group.id).get().then(doc => {
+                    let pubKey = doc.data().publicKey
+                    db.collection("users").where("UserID", "==", firebase.auth().currentUser.uid).get().then(res => {
+                        res.forEach(u => {
+                            let privKey = u.data().privateKey
+
+                            let password = sessionStorage.getItem("password")
+                            let decrypted = CryptoJS.AES.decrypt(privKey, password)
+                            let usersPrivateKey = decrypted.toString(CryptoJS.enc.Utf8)
+
+                            let groupPrivKey = u.data()[this.group.id]
+                            const crypt = new Crypt()
+
+                            let groupPrivateKey = crypt.decrypt(usersPrivateKey, groupPrivKey)
+                            const path = `${this.group.id}/${this.chosenFile.name}`
+
+                            const reader = new FileReader()
+                            reader.onload = function(e) {
+                                let file = e.target.result
+                                let signature = crypt.signature(groupPrivateKey.message, file)
+                                let encryptedFile = crypt.encrypt(pubKey, file, signature)
+                                firebase.storage().ref().child(path).putString(String(encryptedFile))
+                            }
+                            reader.readAsDataURL(this.chosenFile)
+                            setTimeout(this.getFiles, 3000)
+                        })
+                    })
                 })
             }
         },
         getFiles() {
-            let path = this.$route.params.group_id
-            firebase.storage().ref().child(path).listAll().then(res => {
+            firebase.storage().ref().child(this.group.id).listAll().then(res => {
                 res.items.forEach(item => {
-                    this.group.files.push(item)
+                    this.group.files.push(item.name)
                 })
             })
         },
+        download(file){
+            db.collection("groups").doc(this.group.id).get().then(doc => {
+                let pubKey = doc.data().publicKey
+                db.collection("users").where("UserID", "==", firebase.auth().currentUser.uid).get().then(res => {
+                    res.forEach(u => {
+                        let privKey = u.data().privateKey
+                        
+                        let password = sessionStorage.getItem("password")
+                        let decrypted = CryptoJS.AES.decrypt(privKey, password)
+                        let usersPrivateKey = decrypted.toString(CryptoJS.enc.Utf8)
+
+                        let groupPrivKey = u.data()[this.group.id]
+                        const crypt = new Crypt()
+                        let groupPrivateKey = crypt.decrypt(usersPrivateKey, groupPrivKey)
+                        const path = `${this.group.id}/${file}`
+
+                        firebase.storage().ref().child(path).getDownloadURL().then(url => {
+                            let req = new XMLHttpRequest()
+                            req.responseType = 'blob'
+
+                            req.onload = () => {
+                                let blob = req.response
+                                const reader = new FileReader()
+                                reader.onload = function(e){
+                                    const textFile = e.target.result
+                                    let decryptedFile = crypt.decrypt(groupPrivateKey.message, atob(textFile.split(",")[1]))
+                                    let verified = crypt.verify(pubKey, decryptedFile.signature, decryptedFile.message)
+                                    if(!verified){
+                                        alert("FUUUCKKK")
+                                    } 
+                                    else {
+                                        let stringResponse = atob(decryptedFile.message.split(",")[1])
+                                        fileDownload(stringResponse, file)
+                                    }
+                                }
+                                reader.readAsDataURL(blob)
+                            }
+                            req.open("GET", url)
+                            req.send()
+                        })
+                    })
+                })
+            })
+        }
     }
 };
 </script>
